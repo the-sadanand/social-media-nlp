dd487e79f8287eb607ad25968466866e6ae13eb4
SADANAND <thesadanand.toponed0@gmail.com>
2026-09-21 14:51:02 +0530

    src/preprocess.py added: clean_text function

--- src/preprocess.py
+++ src/preprocess.py
@@ -15,3 +15,22 @@ from nltk.tokenize import word_tokenize
 
 for resource in ["punkt","punkt_tab","stopwords","wordnet","omw-1.4"]:
     nltk.download(resource,quiet=True)
+# pre initializing object to avoid repeated constructionl overhead
+_STOP_WORDS:set[str] = set(stopwords.words("english"))
+_LEMMATIZER = WordNetLemmatizer()
+
+def clean_text(text:str) -> str:
+    
+    if not isinstance(text,str):
+        return ""
+    
+    text = re.sub(r"http\S+|www\.\S+", "", text) #urls
+    text = re.sub(r"@\w+","",text) # mentions
+    text = re.sub(r"#\w+","",text) # hastags
+    text = re.sub(r"&\w+;","",text) # html tags
+    text = re.sub(r"[^a-zA-Z\s]","",text) #keep only letters and spaces
+    text = text.lower() #lower
+    text = re.sub(r"\s+","",text) # collaspe white spaces
+    
+    return text 
+    
